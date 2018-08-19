aef96d46c40f78201ad320272aa4457fba07ae75
gistrec <gistrec@mail.ru>
2018-08-20 00:50:42 +0700

    Добавлены пару функций в блок

--- Block.cpp
+++ Block.cpp
@@ -1,5 +1,23 @@
-//
-// Created by alex on 19.08.18.
-//
-
 #include "Block.h"
+
+Block::Block(FILE* filePointer) {
+    // Считываем Magic number
+    fread(magic, sizeof(byte), 4, filePointer);
+
+    if (isMagicValid()) {
+        // Считываем размер блока
+        byte buf[4];
+        fread(buf, sizeof(byte), 4, filePointer);
+        size = getIntFromBytes((byte*) &buf);
+
+        // Считываем заголовок
+        fread(header, sizeof(byte), 80, filePointer);
+
+    }
+
+}
+
+bool Block::isMagicValid() {
+    return magic[0] == 0xf9 && magic[1] == 0xbe &&
+           magic[2] == 0xb4 && magic[3] == 0xd9;
+}
--- Block.h
+++ Block.h
@@ -1,13 +1,18 @@
-//
-// Created by alex on 19.08.18.
-//
-
 #ifndef GENISYS_BLOCK_FROM_UTXO_BLOCK_H
 #define GENISYS_BLOCK_FROM_UTXO_BLOCK_H
 
+#include "main.h"
+#include "utils/Utils.h"
 
 class Block {
+public:
+    byte magic[4];
+    int size;
+    byte header[80];
 
+    Block(FILE* filePointer);
+
+    bool isMagicValid();
 };
 
 
